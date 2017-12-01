fe7de946a63811ad1ec6062c1d05ac0f3836821c
Liz Burton <liz.burton147@gmail.com>
2017-12-01 21:55:06 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,11 +21,8 @@ class CashRegister
   def apply_discount
     if (self.discount > 0)
       self.total = ( self.total *= (100 - self.discount)/100 )
-      # if self.total % 100 == 0
-      #   "After the discount, the total comes to $#{self.total.to_i}."
-      # else
+      self.total % 100 == 0 ? self.total = self.total.to_i : self.total = self.total.round!(2)
         "After the discount, the total comes to $#{self.total}."
-      # end
     else
       "There is no discount to apply."
     end
